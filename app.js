950daf3f00f7dc487eee39af9cb1a30342874a21
Shalaka <waghamaleshamika2013@gmail.com>
2022-06-24 22:16:37 +0530

    Final changes to the Blogsite

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 })
 
-app.listen(5500, function () {
+app.listen(process.env.PORT || 5500, function () {
   // console.log("Server started on port 3000");
 });
